0af12dc7ec1f6b5a4fa0701e821ae2c2b111c468
Yahav Ben Yaakov <yabenyaa@microsoft.com>
2023-04-27 19:19:13 +0300

    initial commit

---
+++ hw2/answers.py
@@ -0,0 +1,128 @@
+r"""
+Use this module to write your answers to the questions in the notebook.
+
+Note: Inside the answer strings you can use Markdown format and also LaTeX
+math (delimited with $$).
+"""
+
+# ==============
+# Part 2 answers
+
+
+def part2_overfit_hp():
+    wstd, lr, reg = 0, 0, 0
+    # TODO: Tweak the hyperparameters until you overfit the small dataset.
+    # ====== YOUR CODE: ======
+    raise NotImplementedError()
+    # ========================
+    return dict(wstd=wstd, lr=lr, reg=reg)
+
+
+def part2_optim_hp(opt_name):
+    wstd, lr, reg, = 0, 0, 0
+
+    # TODO: Tweak the hyperparameters to get the best results you can.
+    # You may want to use different hyperparameters for each optimizer.
+    # ====== YOUR CODE: ======
+    if opt_name == 'vanilla':
+        raise NotImplementedError()
+    if opt_name == 'momentum':
+        raise NotImplementedError()
+    if opt_name == 'rmsprop':
+        raise NotImplementedError()
+    # ========================
+    return dict(wstd=wstd, lr=lr, reg=reg)
+
+
+def part2_dropout_hp():
+    wstd, lr, = 0, 0
+    # TODO: Tweak the hyperparameters to get the model to overfit without
+    # dropout.
+    # ====== YOUR CODE: ======
+    raise NotImplementedError()
+    # ========================
+    return dict(wstd=wstd, lr=lr)
+
+
+part2_q1 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part2_q2 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+# ==============
+
+# ==============
+# Part 3 answers
+
+part3_q1 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part3_q2 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part3_q3 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+
+part3_q4 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
